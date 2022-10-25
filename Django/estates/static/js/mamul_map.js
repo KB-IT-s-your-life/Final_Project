4f605f72c3709355e9778fed292ceafcb1695600
HeeWon <sheewons@naver.com>
2022-10-26 02:36:18 +0900

    css 수정

--- Django/estates/static/js/mamul_map.js
+++ Django/estates/static/js/mamul_map.js
@@ -32,7 +32,7 @@ var mapContainer = document.getElementById("map"), // 지도를 표시할 div
     };
 
 $.getJSON(
-    "http://127.0.0.1:8000/static/json/동시각화 전용.json",
+    "http://localhost:8000/static/json/동시각화 전용.json",
     function (json) {
         var data = json;
 
