4bb242a4edb3c73f281fd9dca276667a15f84a52
LJYGW <105356672+LJYGW@users.noreply.github.com>
2022-10-25 14:48:38 +0900

    Add files via upload

---
+++ Django/views.py
@@ -0,0 +1,101 @@
+from email import contentmanager
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from . models import Facilities, Mamul
+from django.http import JsonResponse
+import joblib
+import json
+import numpy as np
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def base_index_test(request):
+    return render(request, 'base_index_test.html')
+
+def mamul(request):
+    return render(request, 'mamul.html')
+
+def index_taeho(request):
+    return render(request, 'index_taeho.html')
+
+def barchart(request):
+    return render(request, 'barchart.html')
+
+def piechart(request):
+    return render(request, 'piechart.html')
+
+def index_copy(request):
+    return render(request, 'index_copy.html')
+  
+def getfacilities(request):
+    title = request.GET.get('title')
+    facil = Facilities.objects.get(title=title).__dict__
+    del facil['_state']
+
+    context = {
+        'facil':facil,      
+    }
+    return JsonResponse(context)
+
+def getmamuls(request):
+    print('hi')
+    pk = request.GET.get('pk')
+    print(pk)
+    mamuls = Mamul.objects.get(pk=pk).__dict__
+    del mamuls['_state']
+    print(mamuls)
+    context = {
+        'mamuls':mamuls,     
+    }
+    return JsonResponse(context)
+
+def getlatlng(request):
+    print('시작')
+    bozeong = request.GET.get('bozeong')
+    mamul = list(Mamul.objects.filter(bozeonggum__gt = bozeong).values())
+    print(mamul[0])
+
+    context = {
+        'mamul':mamul,      
+    }
+    return JsonResponse(context)
+
+def pca(request):
+    pca_model = joblib.load('/Users/ijung-yun/Final_Project-main/Django/models/pca_tool.pickle')
+    jsonObject = json.loads(request.body)
+    medical = jsonObject.get('medical')
+    facility = jsonObject.get('facility')
+    convenient = jsonObject.get('convenient')
+    market = jsonObject.get('market')
+    leisure = jsonObject.get('leisure')
+    fastfood = jsonObject.get('fastfood')
+    cafe = jsonObject.get('cafe')
+    traffic = jsonObject.get('traffic')
+    restaurant = jsonObject.get('restaurant')
+    shopping = jsonObject.get('shopping')
+    input_list = [medical,facility,convenient,leisure,traffic,restaurant,fastfood,cafe,market,shopping]
+    input_array = np.array(input_list).reshape(1,-1)
+    pca_result = pca_model.transform(input_array)
+    print(pca_result)
+    knn_model = joblib.load('/Users/ijung-yun/Final_Project-main/Django/models/knn_clustering.pkl')
+    pred = knn_model.predict(pca_result)
+    pred = pred.tolist()
+    
+    context = {
+        'pred' : pred
+    }
+    return JsonResponse(context)
+
+
+
+
+# submit 눌렀을때 제출 되게 하는 함수
+# def filter(request):
+
+# #    medical = request.POST.get('medical_val') 
+# #context={
+# #        'medical': medical
+# #    }
+#     #return render(request, 'mamul.html', context)
