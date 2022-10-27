65bd78a343ec855c46c7a34b17626e50ed03d241
HeeWon <sheewons@naver.com>
2022-10-27 20:24:23 +0900

    update

--- Django/estates/static/js/map.js
+++ Django/estates/static/js/map.js
@@ -8,7 +8,7 @@ function home() {
 
     var mapContainer = document.getElementById("map"), // 지도를 표시할 div
         mapOption = {
-            center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+            center: new kakao.maps.LatLng(37.566826, 126.8786567), // 지도의 중심좌표
             level: 9, // 지도의 확대 레벨
         };
 
